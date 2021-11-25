bae5a13f4db90a739a539c176ec428f7aedbdb68
LeonieNollevalle <leonie.nollevalle@gmail.com>
2021-11-25 11:04:04 +0100

    creation composant background+ css background + import dans home

---
+++ src/components/background/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div className="container-background"> 
+      <div className="background-item-1"></div>
+      <div className="background-item-1"></div>
+    </div>
+  )
+}
+
+export default Background
--- src/container/home/Home.jsx
+++ src/container/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Background from '../../components/background/Background';
 
 export default function Home() {
   return (
-    <div>
-      <h1> Accueil </h1>
+    <div className="container-Home">
+      <Background / >
     </div>
   )
 }
