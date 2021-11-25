990c358d5f3aa33ce8741f3ca7dc8dfc8996cd28
angeliquebesi <besineaud85@gmail.com>
2021-11-25 08:21:08 +0100

    trame des routes

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <h1> Kefir </h1>
-      <h2> Qu'est-ce-que le kéfir ?</h2>
-      <p> Le kéfir est une boisson fermentée à base de grains de kefir. Il a des vertus probiotiques.</p>
+      <h1> Accueil </h1>
     </div>
   )
 }
