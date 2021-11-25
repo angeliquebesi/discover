40116fde17b23ed73d8bbd00a6f89d51a2df2b9e
Coucoralie <c.cocourant@gmail.com>
2021-11-25 17:56:48 +0100

    corrige erreur

---
+++ src/data/data.jsx
@@ -0,0 +1,341 @@
+const genres = [
+    {id:0 ,name:"rap"},
+    {id:1, name:"pop"},
+    {id:2, name:"metal"},
+    {id:3, name:"rock"},
+    {id:4, name:"jazz"},
+    {id:5, name:"reggae"},
+    {id:6, name:"classic"},
+    {id:7, name:"electro"},
+    ];
+    
+    const artists = [
+        {id : 0,
+        artist: "ExcluRapEnglish",
+        nb_fan:9,
+        url:"https://soundcloud.com/flaiy-yt",
+        sound:"https://soundcloud.com/flaiy-yt/sch-tempete",
+        image:"https://i1.sndcdn.com/avatars-LuEYCQD5BJTqzdAT-zgYLuQ-t500x500.jpg"
+        },
+    
+        {id : 0,
+        artist: "Daymoliton", 
+        nb_fan:72, 
+        url:"https://soundcloud.com/daymolitonfr", 
+        sound:"https://soundcloud.com/daymolitonfr/douma-kalash-ft-ninho-igo-10-prod-by-binks-beatz", 
+        image:"https://i1.sndcdn.com/avatars-000205012877-uev0v3-t500x500.jpg"
+        },
+    
+        {id : 0, 
+        artist: "GN HIT", 
+        nb_fan:54, 
+        url:"https://soundcloud.com/francois-dupont-415360751", 
+        sound:"https://soundcloud.com/talibandz-entertainment/dino-xiii-tonight", 
+        image:"https://i1.sndcdn.com/avatars-nDZXFNyZun2qAmLZ-BqQbGA-t500x500.jpg"
+        },
+    
+        {id : 0, 
+        artist: "BOXXO_RAP", 
+        nb_fan:99, 
+        url:"https://soundcloud.com/dd45rap", 
+        sound:"https://soundcloud.com/dd45rap/sets/prods", 
+        image:"https://i1.sndcdn.com/avatars-000221743354-a3789n-t500x500.jpg"
+        },
+    
+        {id : 0, 
+        artist: "Blendrap", 
+        nb_fan:319, 
+        url:"https://soundcloud.com/blendrap", 
+        sound:"https://soundcloud.com/angelevl/les-amoureux-des-bancs-publics-angele", 
+        image:"https://i1.sndcdn.com/avatars-000152293061-b50xue-t500x500.jpg"
+        },
+    
+        {id : 1, 
+        artist: "Pineapple", 
+        nb_fan:640, 
+        url:"https://soundcloud.com/pineapplek7", 
+        sound:"https://soundcloud.com/pineapplek7/french-touch-2001", 
+        image:"https://i1.sndcdn.com/avatars-000340534953-xf8o2c-t500x500.jpg"
+        },
+    
+        {id : 1, 
+        artist: "Gilles & Maurice", 
+        nb_fan:248, 
+        url:"https://soundcloud.com/gillesetmaurice", 
+        sound:"https://soundcloud.com/hawhyedits/sigma-fay-tonight-ha-why-edit", 
+        image:"	https://i1.sndcdn.com/avatars-000001768843-n5vjce-t500x500.jpg"
+        },
+    
+        {id : 1, 
+        artist: "WARD", 
+        nb_fan:10, 
+        url:"https://soundcloud.com/wardofficiel", 
+        sound:"https://soundcloud.com/wardofficiel/digital-love-daft-punk-ward-rmx", 
+        image:"https://i1.sndcdn.com/avatars-dYyB1hHRmqsT015I-lp6sFA-t500x500.jpg"
+        },
+        {id : 1, 
+        artist: "Pop'Ash", 
+        nb_fan:33, 
+        url:"https://soundcloud.com/popash", 
+        sound:"https://soundcloud.com/i_d_magazine/music-for-midnight-messages", 
+        image:"https://i1.sndcdn.com/avatars-000062432557-u1010s-t500x500.jpg"
+        },
+    
+        {id : 1, 
+        artist: "Pop In Djerba", 
+        nb_fan:47, 
+        url:"https://soundcloud.com/pop-in-djerba", 
+        sound:"https://soundcloud.com/pop-in-djerba/nawel-den-den-city", 
+        image:"https://i1.sndcdn.com/avatars-000016241415-8iasgm-t500x500.jpg"
+        },
+    
+        {id : 2, 
+        artist: "Arkan OrientalMetal", 
+        nb_fan:119, 
+        url:"https://soundcloud.com/arkan-orientalmetal", 
+        sound:"https://soundcloud.com/arkan-orientalmetal/sets/salam-album", 
+        image:"https://i1.sndcdn.com/avatars-000079343388-wjdt14-t500x500.jpg"
+        },
+    
+        {id : 2, 
+        artist: "Vivien Lalu", 
+        nb_fan:121, 
+        url:"https://soundcloud.com/vivienlalu", 
+        sound:"https://soundcloud.com/rolandcom/celestial-spheres-demo", 
+        image:"https://i1.sndcdn.com/avatars-pWzz7bu2pkh4JQuk-u5yUkw-t500x500.jpg"
+        },
+    
+        {id : 2, 
+        artist: "Metal Ninja", 
+        nb_fan:148, 
+        url:"https://soundcloud.com/metal-ninja", 
+        sound:"https://soundcloud.com/metal-ninja/craft-chase", 
+        image:"https://i1.sndcdn.com/avatars-000001668199-uurnyl-t500x500.jpg"
+        },
+    
+        {id : 2,
+        artist: "Gargantua Metal", 
+        nb_fan:38, 
+        url:"https://soundcloud.com/gargantuametal", 
+        sound:"https://soundcloud.com/gargantuametal/threshold-of-death-gutters", 
+        image:"https://i1.sndcdn.com/avatars-000115487097-p9s2zm-t500x500.jpg"
+        },
+    
+        {id : 2, 
+        artist: "NeoMetal", 
+        nb_fan:45, 
+        url:"https://soundcloud.com/neometal-1", 
+        sound:"https://soundcloud.com/neometal-1/neoskream-back-in-black", 
+        image:"https://i1.sndcdn.com/avatars-000047383033-8z9wl2-t500x500.jpg"
+        },
+    
+        {id : 3,
+        artist: "Music Events Agency", 
+        nb_fan:83, 
+        url:"https://soundcloud.com/musiceventsagency", 
+        sound:"https://soundcloud.com/musiceventsagency/medley-hegemony", 
+        image:"https://i1.sndcdn.com/avatars-000113721951-tjv1kk-t500x500.jpg"
+        },
+    
+        {id : 3, 
+        artist: "RockVibes'studio",
+        nb_fan:3,
+        url:"https://soundcloud.com/user-881870153-907578065",
+        sound:"https://soundcloud.com/user-881870153-907578065/clichas-de-sociata_mixv2_5",
+        image:"https://i1.sndcdn.com/avatars-000132283578-aoa5fn-t500x500.jpg"
+        },
+    
+        {id : 3, 
+        artist: "Zoran Rock",
+        nb_fan:9,
+        url:"https://soundcloud.com/user-847846969",
+        sound:"https://soundcloud.com/user-847846969/sets/fire-at-corbeil",
+        image:"https://i1.sndcdn.com/avatars-000465224130-0awapj-t500x500.jpg"
+        },
+    
+        {id : 3, 
+        artist: "BlackRain Rock",
+        nb_fan:9,
+        url:"https://soundcloud.com/blackrain-rock",
+        sound:"https://soundcloud.com/blackrain-rock/the-middle-jimmy-eat-world-cover",
+        image:"https://i1.sndcdn.com/avatars-000094312880-8f1v17-t500x500.jpg"
+        },
+    
+        {id : 3, 
+        artist: "Nilo Griffin",
+        nb_fan:41,
+        url:"https://soundcloud.com/nilogriffin",
+        sound:"https://soundcloud.com/nilogriffin/up-for-the-challenge-v1",
+        image:"https://i1.sndcdn.com/avatars-000882983509-aw5nr9-t500x500.jpg"
+        },
+    
+        {id : 4, 
+        artist: "Jazz&People",
+        nb_fan:436,
+        url:"https://soundcloud.com/jazz-people",
+        sound:"https://soundcloud.com/jazz-people/01-le-a-est-lasse",
+        image:"https://i1.sndcdn.com/avatars-000149939388-xgfqya-t500x500.jpg"
+        },
+    
+        {id : 4, 
+        artist: "ColoreJazz",
+        nb_fan:43,
+        url:"https://soundcloud.com/colorejazz",
+        sound:"https://soundcloud.com/colorejazz/davic-linx-kiss-the-ground",
+        image:"https://i1.sndcdn.com/avatars-000069459843-554yzl-t500x500.jpg"
+        },
+    
+        {id : 4, 
+        artist: "Sayag Jazz Machine",
+        nb_fan:421,
+        url:"https://soundcloud.com/sayag-jazz-machine",
+        sound:"https://soundcloud.com/sayag-jazz-machine/noskool",
+        images:"	https://i1.sndcdn.com/avatars-000001939803-ewh1fu-t500x500.jpg"
+        },
+    
+        {id : 4, 
+        artist: "L'épicerie du jazz",
+        nb_fan:58,
+        url:"https://soundcloud.com/epiceriedujazz",
+        sound:"https://soundcloud.com/epiceriedujazz/jardin-dhiver",
+        image:"https://i1.sndcdn.com/avatars-PASsZWuakSgHd7ht-U6msjg-t500x500.jpg"
+        },
+    
+        {id : 4, 
+        artist: "TSF JAZZ",
+        nb_fan:90,
+        url:"https://soundcloud.com/tsf-jazz-1",
+        sound:"https://soundcloud.com/tsf-jazz-1/entretien-avec-jean-rochard-fondateur-de-la-maison-de-disques-nato",
+        image:"https://i1.sndcdn.com/avatars-000322383608-z6wmsi-t500x500.jpg"
+        },
+    
+        {id : 5, 
+        artist: "Vibzone.fr",
+        nb_fan:352,
+        url:"https://soundcloud.com/vibzone",
+        sound:"https://soundcloud.com/vibzone/kalash-don-kalash",
+        image:"	https://i1.sndcdn.com/avatars-000019098606-p7rylb-t500x500.jpg"
+        },
+    
+        {id : 5, 
+        artist: "Le sel",
+        nb_fan:41,
+        url:"https://soundcloud.com/lesel",
+        sound:"https://soundcloud.com/lesel/sets/brown-scene-45t",
+        image:"https://i1.sndcdn.com/avatars-000219393836-cop236-t500x500.jpg"
+        },
+    
+        {id : 5, 
+        artist: "SUNDYATA",
+        nb_fan:63,
+        url:"https://soundcloud.com/sundyata",
+        sound:"https://soundcloud.com/sundyata/goal",
+        image:"https://i1.sndcdn.com/avatars-000023535205-875yjo-t500x500.jpg"
+        },
+    
+        {id : 5, 
+        artist: "Reggae Acoustic Club",
+        nb_fan:321,
+        url:"https://soundcloud.com/reggae-acoustic-club",
+        sound:"https://soundcloud.com/reggae-acoustic-club/conquering-sound-system-mix-promo-rac-1",
+        image:"	https://i1.sndcdn.com/avatars-000002330692-bkl9he-t500x500.jpg"
+        },
+    
+        {id : 5, 
+        artist: "Reggae Champion's League",
+        nb_fan:27,
+        url:"https://soundcloud.com/reggaechampionsleague",
+        sound:"https://soundcloud.com/reggaechampionsleague/mehdi-vs-yoda-part3-worldjam",
+        image:"https://i1.sndcdn.com/avatars-000044302070-hobz7z-t500x500.jpg"
+        },
+    
+        {id : 6, 
+        artist: "Classic as Fuck",
+        nb_fan:743,
+        url:"https://soundcloud.com/classicasfuck",
+        sound:"https://soundcloud.com/figure-official/sets/figure-77-faki-regal-the-end",
+        image:"https://i1.sndcdn.com/avatars-000028994400-laartn-t500x500.jpg"
+        },
+    
+        {id : 6, 
+        artist: "Classic State (aka Steek)",
+        nb_fan:28,
+        url:"https://soundcloud.com/classic-state",
+        sound:"https://soundcloud.com/classic-state/addicted-to-love-cs-re-vision",
+        image:"https://i1.sndcdn.com/avatars-000007046804-jezrbj-t500x500.jpg"
+        },
+    
+        {id : 6, 
+        artist: "Flashback",
+        nb_fan:120,
+        url:"https://soundcloud.com/flashbackpodcast",
+        sound:"https://soundcloud.com/flashbackpodcast/28-entretien-avec-jerzy-2",
+        image:"https://i1.sndcdn.com/avatars-000629014008-0863zy-t500x500.jpg"
+        },
+    
+        {id : 6, 
+        artist: "CHREESTYLE",
+        nb_fan:130,
+        url:"https://soundcloud.com/chreestyle",
+        sound:"https://soundcloud.com/gvo-458714030/sets/chreestyle-versatil",
+        image:"https://i1.sndcdn.com/avatars-ezBBwro3FCNydaW0-bwHAvA-t500x500.jpg"
+        },
+    
+        {id : 6, 
+        artist: "The Soulfingers",
+        nb_fan:567,
+        url:"https://soundcloud.com/thesoulfingers",
+        sound:"https://soundcloud.com/thesoulfingers/groove-odyssey-94-barry-white-daft-punk-melba-moore-earth-wind-fire-more",
+        image:"https://i1.sndcdn.com/avatars-000719276116-9vs6ew-t500x500.jpg"
+        },
+    
+        {id : 7, 
+        artist: "THE ELECTRONIC CONSPIRACY",
+        nb_fan:776,
+        url:"https://soundcloud.com/theelectronicconspiracy",
+        sound:"https://soundcloud.com/theelectronicconspiracy/falling-down-1",
+        image:"https://i1.sndcdn.com/avatars-000114394977-m0x3pg-t500x500.jpg"
+        },
+    
+        {id : 7, 
+        artist: "Arnaud Millet",
+        nb_fan:847,
+        url:"https://soundcloud.com/arnaudmillet",
+        sound:"https://soundcloud.com/arnaudmillet/sets/bay-vibes-radio-tech-control",
+        image:"https://i1.sndcdn.com/avatars-GOuZOrVm0n7sfUqD-mghn9Q-t500x500.jpg"
+        },
+    
+        {id : 7, 
+        artist: "Nella.S",
+        nb_fan:661,
+        url:"https://soundcloud.com/nylsie",
+        sound:"https://soundcloud.com/nylsie/nella-s-mon-loup-radio-edit",
+        images:"https://i1.sndcdn.com/avatars-uRjKFaGpz1vx1zal-NRIulw-t500x500.jpg"
+        },
+    
+        {id : 7, 
+        artist: "FENOFLOW",
+        nb_fan:171,
+        url:"https://soundcloud.com/fenoflow",
+        sound:"https://soundcloud.com/fenoflow/vengaboys-feat-fenoflow-boom-boom-remix-2017",
+        images:"https://i1.sndcdn.com/avatars-000306287342-vp1djy-t500x500.jpg"
+        },
+    
+        {id : 7, 
+        artist: "Electro Clash 2000",
+        nb_fan:101,
+        url:"https://soundcloud.com/user9433919",
+        sound:"https://soundcloud.com/user9433919/un-point-cest-miou",
+        image:"https://i1.sndcdn.com/avatars-000144039819-97wiv5-t500x500.jpg"
+        },
+    ]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
