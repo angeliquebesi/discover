2406e973706f5480b7868da37e004df8307a520a
Camille Hautois <camille.hautois@gmail.com>
2021-11-25 18:12:29 +0100

    Add route to genre page + tentative reglase css (pb largeur images

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/navbar/NavBar";
 import Home from "./container/home/Home";
 import Genre from "./container/genre/Genre";
-import Musique from "./container/musique/Musique";
 import Contact from "./container/contact/Contact";
+import SwipCube from "./components/swip/Cube";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/genre" element={<Genre />} />
-        <Route exact path="/genre/musique" element={<Musique />} />
+        <Route exact path="/genre/:id" element={<SwipCube />} />
         <Route exact path="/artiste" element={<Contact />} />
       </Routes>
     </div>
--- src/container/genre/Genre.jsx
+++ src/container/genre/Genre.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../../components/navbar/NavBar";
 import Genres from "./data"; // importer le fichier data de Coralie à la place
 import "./Genre.css";
 
 export default function Genre() {
   console.log(Genres);
   return (
-    <>
-      {Genres.map((genre) => (
-        <div className={`${genre.name} categorie`}>
-          <h2>{genre.name}</h2>
-        </div>
-      ))}
-    </>
+    <Link to="/genre/:id/">
+      <>
+        <NavBar />
+        {Genres.map((genre) => (
+          <div className={`${genre.name} categorie`}>
+            <h2>{genre.name}</h2>
+          </div>
+        ))}
+      </>
+    </Link>
   );
 }
