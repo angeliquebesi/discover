698c58c006f839f486285e9ffac46a8b27163d3e
Camille Hautois <camille.hautois@gmail.com>
2021-11-25 08:38:45 +0100

    resolution conflits

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-import {Link} from 'react-router-dom';
-=======
 import { Link } from 'react-router-dom';
->>>>>>> f156485b2559b1ba53a7348390574ce51fa1db16
 
 export default function NavBar() {
   return (
     <div>
       <ul>
-<<<<<<< HEAD
-        <Link to="/">Accueil</Link><br/>
-        <Link to="/recettes">Recettes </Link><br/>
-        <Link to="/gestionnaire">Gestionnaire </Link><br/>
-        <Link to="/troc">Troc </Link>
-      </ul>
-    </div>
-  )
-}
-=======
         <Link to="/">Accueil</Link><br />
         <Link to="/gestionnaire">Gestionnaire </Link><br />
       </ul>
     </div>
   )
 }
->>>>>>> f156485b2559b1ba53a7348390574ce51fa1db16
